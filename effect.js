f9b72474995675ab70a67c9395dca49cb8fd19bf
shivansh-rajput-01 <Shivanshr579@gmail.com>
2024-12-19 20:03:59 +0530

    adding my portfolio files

---
+++ effect.js
@@ -0,0 +1,21 @@
+let symbol=document.querySelectorAll(".symbol");
+let bar=document.querySelectorAll(".bar");
+let newBox=document.querySelectorAll(".newBox");
+let mode="closed";
+for(let i=0;i<3;i++){
+    bar[i].addEventListener('click',()=>{
+        if(mode==="closed"){
+            mode="open";
+            symbol[i].innerText="x";
+            newBox[i].classList.remove("hidden");
+            symbol[i].style.fontWeight=400;
+            bar[i].style.marginBottom="0px";
+        }else{
+            mode="closed";
+            symbol[i].innerText="+";
+            newBox[i].classList.add("hidden");
+            symbol[i].style.fontWeight=900;
+            bar[i].style.marginBottom="20px";
+        }
+    });
+}
